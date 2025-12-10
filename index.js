91eb3876e8a043a4970a6b3305677fa595367cf4
Riccardo Randazzo <riccardorandazzo8@gmail.com>
2025-12-10 10:08:00 +0100

    modified

--- index.js
+++ index.js
@@ -16,4 +16,64 @@ function getEvenNumbers(arr) {
 }
 console.log(even);
 
-const 
+// Esercizio 7
+/*function getLengthFromArr(array) {
+  const newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const str = array[i];
+
+    newArr.push(str.length);
+  }
+  return newArr;
+}*/
+
+console.log(getLengthFromArr(["EPICODE", "is", "great"]));
+
+const getLengthFromArr = (array) => array.map((str) => str.length);
+console.log(getLengthFromArr(["EPICODE", "is", "great"]));
+
+//Esercizio 9
+const getOldestMovie = function (array) {
+  let getOldestMovie = array[0];
+
+  array.forEach((movie) => {
+    let getOldestMovie = array[0];
+
+    if (movie.Year < getOldestMovie.Year) {
+      getOldestMovie = movie;
+    }
+  });
+  return getOldestMovie;
+};
+console.log(getOldestMovie(movies));
+
+//Esercizio 10
+const getLengthFromMovies = function (array) {
+  return array.length;
+};
+console.log(getLengthFromMovies(movies));
+
+//Esercizio 11
+const onlyTitles = function (array) {
+  return array.map((movie) => movie.Title);
+};
+console.log(onlyTitles(movies));
+
+//Eesercizio 12
+const onlyInCurrentMillenium = function (array) {
+  return array.filter((movie) => movie.Year >= 2000);
+};
+console.log(onlyInCurrentMillenium(movies));
+
+//Esercizio 14
+const findMovieById = function (id) {
+  return movies.find((movie) => movie.imdbID === id);
+};
+console.log(findMovieById("tt0087365"));
+
+//Esercizio 15
+const findAndDelete = function (array, year) {
+  return array.findIndex((movie) => movie.Year === year);
+};
+console.log(findAndDelete(movies, "2000"));
